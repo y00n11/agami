6f33b00b3ab9fd83ca2b28f15e9ad1227b420571
y00n <YYYyyy@hywoman.ac.kr>
2026-08-11 01:14:18 +0900

    오디오 음성 다운 막음 38

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const strips = document.querySelectorAll('.strip-item');
     strips.forEach(strip => {
+
+        //3번까지 엥간하면 수정 ㄴㄴ... 수정할수록 카드이미지나 링크나 공유가 누락됨..
         strip.addEventListener('click', function() {
             // 1. 번호표 뜯어지는 애니메이션 실행
             this.classList.add('torn');
 
             // 2. 트윗 본문 및 링크 준비
             const tweetText = encodeURIComponent(
-                "인어왕자 보신 분? 여기로 연락달라네요\n" +
+                "인어왕자 보신 분? 여기로 제보해주세요n" +
+                // "마지막 목격 장소도 참고 ㄱㄱ \n"
                 ">> @heisg0ne \n\n" +
                 ">>26.09.09 개봉 #아가미 #구병모"
             );
